b9f02d91e0f1c0c7571a83ceb4ed645ade587f4f
sonhai1088 <sonhai1088@gmail.com>
2019-10-11 17:06:05 +0700

    Test

--- WindHome/routes/web.php
+++ WindHome/routes/web.php
@@ -18,10 +18,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+<<<<<<< HEAD
 
 Route::group(['prefix' => 'comments'], function(){
     Route::get('/', 'CommentController@index')->name('comment.index');
     Route::get('/create', 'CommentController@create')->name('comment.create');
     Route::post('/create', 'CommentController@store')->name('comment.store');
 
+=======
+Route::group(['prefix' => 'rooms'], function () {
+    Route::get('/', 'RoomController@index')->name('room.index');
+    Route::get('/create', 'RoomController@create')->name('room.create');
+    Route::post('/create', 'RoomController@store')->name('room.store');
+    Route::get('/update/{id}', 'RoomController@edit')->name('room.edit');
+    Route::post('/update/{id}', 'RoomController@update')->name('room.update');
+    Route::get('/delete/{id}', 'RoomController@destroy')->name('room.destroy');
+    Route::get('/detail/{id}', 'RoomController@show')->name('room.detail');
+>>>>>>> f03aeefc621287ed6509cc4ada6dc50acffad35e
 });
