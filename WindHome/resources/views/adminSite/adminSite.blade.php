fa7b33bfbf4a8c737ad833203867a3d513f69f0b
hai <im.hai.89@gmail.com>
2019-10-11 15:40:19 +0700

    Add giao dien adminSite

---
+++ WindHome/resources/views/adminSite/adminSite.blade.php
@@ -0,0 +1,60 @@
+@extends('layout.home')
+@section('sideBar')
+    @include('adminSite.sideBarAdmin')
+@endsection
+
+@section('content')
+    <!-- Page Content  -->
+    <div id="content">
+
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Tất cả các phòng</h2>
+            </div>
+            <div class="col-md-6">
+                <form id="test" class="form-inline" style="float: right">
+                    <input class="form-control mr-sm-2" type="search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm Kiếm</button>
+                </form>
+            </div>
+
+        </div>
+
+
+        <div class="row pt-5">
+            <table class="table table-hover">
+                <thead class="thead-light">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry the Bird</td>
+                    <td>Thornton</td>
+                    <td>@twitter</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <button class="btn btn-outline-success" type="submit">Create</button>
+
+
+    </div>
+@endsection
