e3a43ea3c0bc4179fa51fee83f495dc910a1186f
sonhai1088 <sonhai1088@gmail.com>
2019-10-15 17:06:52 +0700

    Up phần Detail

--- WindHome/resources/views/listSite/roomDetail.blade.php
+++ WindHome/resources/views/listSite/roomDetail.blade.php
@@ -3,11 +3,12 @@
 @section('content')
 
     <div class="container">
+
         <div class="row pt-5">
             <div class="col-md-7">
 
 
-                <img src="https://img.kpopmap.com/2019/10/blackpink-o-lens-cover.jpg" class="img-fluid" alt="Responsive image">
+                <img src={{asset("storage/img/home/". $room->image)}} class="img-fluid" alt="Responsive image">
             </div>
 
             <div class="col-md-5">
@@ -15,15 +16,19 @@
 
                 <form class="bg-white p-3">
                     <div>
-                        <h2 class="font-weight-bold text-success"> Phong A24</h2>
-                        <label> Số 15, TT04, Hàm Nghi, Hà Nội, Việt Nam</label>
+                        <h2 class="font-weight-bold text-success"> {{$room->name}}</h2>
+                        <label> {{$room->address}}, {{ $room->city }}</label>
                         <hr>
                     </div>
                     <div>
-                        <label> pricePerMonth </label><br>
-                        <label> area </label><br>
-                        <label> minRentTime </label><br>
-                        <label> status </label><br>
+                        <label> Giá phòng: {{$room->pricePerMonth}} VNĐ/tháng </label><br>
+                        <label> Diện tích: {{$room->area}} m2 </label><br>
+                        <label> Thời gian thuê tối thiểu: {{$room->minRentTime}} tháng </label><br>
+                        <label> Trạng thái:
+                            @if (isset($room->status)) Còn phòng
+                            @else Hết phòng
+                            @endif
+                        </label><br>
 
                         <hr>
                     </div>
@@ -31,56 +36,62 @@
                     <div class="row pt-2">
                         <div class="wrapper col-md-4">
                             <i class="fas fa-bath fa-1x"></i>
-                            <div class="pl-2"> Có </div> {{--bathRoom--}}
+                            <div class="pl-2">
+                                @if (isset($room->bathRoom)) Có
+                                @else Không
+                                @endif
+                            </div> {{--bathRoom--}}
                         </div>
                         <div class="wrapper col-md-4">
                             <div class=""><i class="fas fa-user"></i></div>
-                            <div class="pl-2">4</div> {{--guest--}}
+                            <div class="pl-2">{{$room->guest}}</div> {{--guest--}}
                         </div>
                         <div class="wrapper col-md-4">
                             <i class="fas fa-parking"></i>
-                            <div class="pl-2">4</div> {{--parking--}}
+                            <div class="pl-2">
+                                @if (isset($room->parking)) Có
+                                @else Không
+                                @endif
+                            </div> {{--parking--}}
                         </div>
                     </div>
                     <div class="row pt-2">
                         <div class="wrapper col-md-4">
                             <i class="fas fa-wifi"></i>
-                            <div class="pl-2">3</div> {{--wifi--}}
+                            <div class="pl-2">
+                                @if (isset($room->wifi)) Có
+                                @else Không
+                                @endif
+                            </div> {{--wifi--}}
                         </div>
                         <div class="wrapper col-md-4">
                             <i class="fas fa-utensil-spoon"></i>
-                            <div class="pl-2">4</div> {{--cooking--}}
+                            <div class="pl-2">
+                                @if (isset($room->cooking)) Có
+                                @else Không
+                                @endif
+                            </div> {{--cooking--}}
                         </div>
 
 
                         <div class="wrapper col-md-4">
                             <i class="fas fa-snowflake"></i>
-                            <div class="pl-2">4</div> {{--airCondition--}}
-                        </div>
-                    </div>
-                    <div class="row pt-2">
-                        <div class="wrapper col-md-4">
-                            <div class=""><i class="fas fa-user"></i></div>
-                            <div class="pl-2">3</div> {{--Số người--}}
-                        </div>
-                        <div class="wrapper col-md-4">
-                            <div class=""><i class="fas fa-star"></i></div>
-                            <div class="pl-2">4</div> {{--rating--}}
-                        </div>
-                        <div class="wrapper col-md-4">
-                            <div class=""><i class="fas fa-star"></i></div>
-                            <div class="pl-2">4</div> {{--rating--}}
+                            <div class="pl-2">
+                                @if (isset($room->airCondition)) Có
+                                @else Không
+                                @endif
+                            </div> {{--airCondition--}}
                         </div>
                     </div>
                     <hr>
                     <div class="pt-2">
                         <label>Thời Gian Thuê</label>
                         <div class="input-group input-group-sm mb-3">
-                            <input type="number" class="form-control"  aria-describedby="inputGroup-sizing-sm">
+                            <input type="number" class="form-control" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </div>
                     <div class="pt-2">
-                        <button type="button" class="btn btn-success btn-lg btn-block">BOOK EM ĐI ANH</button>
+                        <button type="button" class="btn btn-success btn-lg btn-block">ĐẶT PHÒNG</button>
                     </div>
 
                 </form>
