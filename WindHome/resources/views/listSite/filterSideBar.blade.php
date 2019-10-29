f298c3dabfd50e0665cf1d29ee891bfb85438a6c
hai <im.hai.89@gmail.com>
2019-10-29 16:48:16 +0700

    hai add file

--- WindHome/resources/views/listSite/filterSideBar.blade.php
+++ WindHome/resources/views/listSite/filterSideBar.blade.php
@@ -1,43 +1,50 @@
-<nav id="sidebar" style="visibility: visible">
-    <div class="sidebar-header">
-        <a href="https://www.google.com"><h3>Wind Home</h3></a>
+
+
+<nav id="sidebar" style="visibility: visible ; background: #dee2e6; color: #1b1e21 "  >
+    <div class="sidebar-header" style="background: #dee2e6; color: #1b1e21">
+        <a href="https://www.google.com"><h3>Lọc Tìm Kiếm</h3></a>
     </div>
+    <hr>
 
-    <ul class="list-unstyled components">
-        <p>Admin Site</p>
-        {{--            Phòng--}}
-        <li class="active">
-            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Phòng</a>
-            <ul class="collapse list-unstyled" id="homeSubmenu">
-                <li>
-                    <a href="{{route('room.list')}}">Tất cả</a>
-                </li>
-                <li>
-                    <a href="#">Đang cho thuê</a>
-                </li>
-                <li>
-                    <a href="{{route('room.create')}}">Thêm phòng mới</a>
-                </li>
-            </ul>
-        </li>
-        {{-- quản lí user--}}
-        <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Khách
-                hàng</a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
-                <li>
-                    <a href="#">Tất cả</a>
-                </li>
-                <li>
-                    <a href="#">Đang thuê nhà</a>
-                </li>
-            </ul>
-        </li>
-        {{--Comment--}}
-        <li>
-            <a href="#">Comment</a>
-        </li>
+    <form>
+        <div class="form-group">
+            <label for="formControlRange">Example Range input</label>
+            <input type="range" class="form-control-range" id="formControlRange">
+        </div>
+    </form>
 
 
-    </ul>
+
+
+
+{{--        <li class="active">--}}
+{{--            <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Phòng</a>--}}
+{{--            <ul class="collapse list-unstyled" id="homeSubmenu">--}}
+{{--                <li>--}}
+{{--                    <a href="{{route('room.list')}}">Tất cả</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#">Đang cho thuê</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="{{route('room.create')}}">Thêm phòng mới</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </li>--}}
+{{--        --}}{{-- quản lí user--}}
+{{--        <li>--}}
+{{--            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Khách--}}
+{{--                hàng</a>--}}
+{{--            <ul class="collapse list-unstyled" id="pageSubmenu">--}}
+{{--                <li>--}}
+{{--                    <a href="#">Tất cả</a>--}}
+{{--                </li>--}}
+{{--                <li>--}}
+{{--                    <a href="#">Đang thuê nhà</a>--}}
+{{--                </li>--}}
+{{--            </ul>--}}
+{{--        </li>--}}
+{{--        --}}{{--Comment--}}
+
+
 </nav>
